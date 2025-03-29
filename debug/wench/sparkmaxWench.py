46f90c6fcb1b93316f2cf4dac3961f1010a9ec80
BC Robotics <bcrobotics4504@gmail.com>
2025-03-29 11:18:36 -0400

    commit wench

---
+++ debug/wench/sparkmaxWench.py
@@ -0,0 +1,106 @@
+import math
+import rev
+
+
+class SparkMaxWench:
+    """Swerve Drive SparkMax Class
+    Custom class for configuring SparkMaxes used in Swerve Drive Drivetrain
+    """
+
+    # PID coefficients
+    kP = 0.15
+    kI = 0
+    kD = 0
+    kIz = 0
+    kFF = 0.0
+    kMaxOutput = 2 * math.pi
+    kMinOutput = -2 * math.pi
+    maxRPM = 5700
+
+    # Smart Motion Coefficients
+    maxVel = 1000  # rpm
+    maxAcc = 600
+    minVel = 0
+    allowedErr = 0
+
+    smartMotionSlot = 0
+
+    def __init__(
+        self,
+        canID,
+        inverted=False,
+        gear_ratio=1,
+        wheel_diameter=1,
+        absolute_encoder=False,
+        z_offset=0,
+    ):
+        self.canID = canID
+        self.gear_ratio = gear_ratio
+        self.inverted = inverted
+        self.absolute = absolute_encoder
+        self.gear_ratio = gear_ratio
+        self.wheel_diameter = wheel_diameter
+        self.zOffset = z_offset
+
+        self.motor = rev.SparkMax(self.canID, rev.SparkMax.MotorType.kBrushless)
+        self.config = rev.SparkMaxConfig()
+
+        # self.config.smartCurrentLimit(20)
+
+        self.config.absoluteEncoder.setSparkMaxDataPortConfig()
+        self.config.absoluteEncoder.inverted(not inverted)
+        self.config.absoluteEncoder.endPulseUs(1024)
+        self.config.absoluteEncoder.startPulseUs(1)
+        self.config.absoluteEncoder.positionConversionFactor(2 * math.pi)
+
+        # self.config.IdleMode(rev.SparkMax.IdleMode.kBrake)
+        z_offset /= 2.0 * math.pi
+        if z_offset < 0.0:
+            z_offset += 1.0
+
+        self.config.absoluteEncoder.zeroOffset(
+            z_offset
+        )  #!FIXME Causes code to crash with Invalid Parameter Runtime error
+        # #TODO: Configure Feedback Sensor dataport
+        self.config.closedLoop.setFeedbackSensor(
+            rev.ClosedLoopConfig.FeedbackSensor.kAbsoluteEncoder
+        )
+        self.config.closedLoop.positionWrappingEnabled(True)
+        self.config.closedLoop.positionWrappingMinInput(0)
+        self.config.closedLoop.positionWrappingMaxInput(2 * math.pi)
+        self.config.closedLoop.pidf(self.kP, self.kI, self.kD, self.kFF)
+        self.config.closedLoop.outputRange(
+            self.kMinOutput, self.kMaxOutput, rev.ClosedLoopSlot.kSlot0
+        )
+
+        self.motor.configure(
+            self.config,
+            rev.SparkMax.ResetMode.kResetSafeParameters,
+            rev.SparkMax.PersistMode.kPersistParameters,
+        )
+        self.encoder = self.motor.getAbsoluteEncoder()
+        self.controller = self.motor.getClosedLoopController()
+        self.clearFaults()
+
+    def clearFaults(self):
+        """SparkMaxTurning.clearFaults()
+
+        Clears the faults of the turning SparkMax
+        """
+        self.motor.clearFaults()
+
+    def setAbsPosition(self, position: float):
+        """SparkMaxTurning.setAbsPosition()
+
+        Sets the absoulute positon of the encoder"""
+        self.controller.setReference(
+            position, rev._rev.SparkLowLevel.ControlType.kPosition
+        )
+        return False
+
+    def getAbsPosition(self):
+        """SparkMaxTurning.getAbsPosition()
+
+        Gets the absolute positon of the encoder
+        """
+        return self.encoder.getPosition()
---
+++ debug/wench/wench.py
@@ -0,0 +1,60 @@
+from enum import Enum
+
+from collections import namedtuple
+from .sparkmaxWench import SparkMaxWench
+
+WenchConfig = namedtuple('config',
+                          ['CAN_id',
+                           'zoffset',
+                           'gear_ratio',
+                           'up_angle',
+                           'down_angle'])
+
+class WenchPosition(Enum):
+    UP = 1
+    DOWN = 2
+
+class Wench:
+    config: WenchConfig
+    wenchMotor: SparkMaxWench
+    
+    current_position: WenchPosition = WenchPosition.UP
+    
+    def __init__(self, config: WenchConfig) -> None:
+        self.config = config
+        
+        
+        self.wenchMotor = SparkMaxWench(
+            self.config.CAN_id, 
+            inverted=False, 
+            absolute_encoder=True, 
+            z_offset=self.config.gear_ratio
+        )
+        
+    def setDown(self):
+        if self.current_position != WenchPosition.DOWN:
+            self.__setAngle__(self.config.down_angle)
+            self.current_position = WenchPosition.DOWN
+        return
+    
+    def setUp(self):
+        if self.current_position != WenchPosition.UP:
+            self.__setAngle__(self.config.up_angle)
+            self.current_position = WenchPosition.UP
+        return
+        
+    def __setAngle__(self, angle: float):
+        self.wenchMotor.setAbsPosition(angle*self.config.gear_ratio)
+        return False
+
+    def __getAngle__(self):
+        return self.wenchMotor.getAbsPosition()
+    
+    def clearFaults(self):
+        self.wenchMotor.clearFaults()
+        return None
+    
+    def resetEncoder(self):
+        self.wenchMotor.getEncoder().setPosition(0)
+
+        
